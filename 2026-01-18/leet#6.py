e734d0f26dfc8b8dc86a77efe276ed19bcc632ef
Eshwarpawanpeddi <eshwarpavanpeddi@gmail.com>
2026-01-18 17:13:08 +0530

    concatination corrections

--- 2026-01-18/leet#6.py
+++ 2026-01-18/leet#6.py
@@ -10,5 +10,5 @@ class Solution:
             my_list = my_list[::-1]
             if my_list[0] =="0":
                 my_list.pop(0)
-            final_ans = symbol + my_list
+            final_ans = int(symbol + ''.join(my_list))
         return final_ans
