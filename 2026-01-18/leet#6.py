fcb17492b2a580d31f0a65e8568eaa9302b98a98
Eshwarpawanpeddi <eshwarpavanpeddi@gmail.com>
2026-01-18 17:19:48 +0530

    final updates

--- 2026-01-18/leet#6.py
+++ 2026-01-18/leet#6.py
@@ -1,16 +1,17 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        my_list = [x]
-        if len(my_list) > 0:
+        my_list = list(str(x))
+        
+        symbol = ""
+        
+        if len(my_list) > 0: 
             if my_list[0] == '-':
                 symbol = "-"
                 my_list.pop(0)
-            else:
-                symbol = ""
             my_list = my_list[::-1]
-            if my_list[0] =="0":
-                my_list.pop(0)
-            final_ans = []
-            final_ans.append(symbol)
-            final_ans.join(my_list)]
-        return final_ans
+            final_str = symbol + "".join(my_list)
+            result = int(final_str)
+            if result < -2**31 or result > 2**31 - 1:
+                return 0
+                
+            return result
