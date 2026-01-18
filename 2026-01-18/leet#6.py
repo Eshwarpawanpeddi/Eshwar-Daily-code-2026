0d9ef871193659337cf284fd69afa04147d708e2
Eshwarpawanpeddi <eshwarpavanpeddi@gmail.com>
2026-01-18 17:07:47 +0530

    first atempt

--- 2026-01-18/leet#6.py
+++ 2026-01-18/leet#6.py
@@ -1,3 +1,12 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        
+        my_list = list(x)
+        if len(my_list) > 0:
+            if my_list[0] == '-':
+                symbol = "-"
+                my_list.pop(0)
+            my_list = my_list[::-1]
+            if my_list[0] =="0":
+                my_list.pop(0)
+        final_ans = symbol + my_list
+        return final_ans
