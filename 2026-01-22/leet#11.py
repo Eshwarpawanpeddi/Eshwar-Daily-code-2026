d6547d1202efdb6ed2a53451252603dc3d371e41
Eshwarpawanpeddi <eshwarpavanpeddi@gmail.com>
2026-01-22 21:14:38 +0530

    few changes

--- 2026-01-22/leet#11.py
+++ 2026-01-22/leet#11.py
@@ -1,14 +1,21 @@
 class Solution:
-    def area(self,i,j):
-        return i*j
+    def area(self,leng,i,j):
+        if i<j:
+            count = j
+        else:            
+            count = i
+        return count * leng
 
     def maxArea(self, height: List[int]) -> int:
         left = 0
         right = 0
         max_area = 0
+        leng = 0
+
         for i in range(len(height)):
             for j in range(len(height)):
-                temp_area = self.area(height[i], height[j])
+                leng = j - i
+                temp_area = self.area(leng,height[i], height[j])
                 if temp_area > max_area:
                     max_area = temp_area
         return max_area
