6966753fcdbfeaf70573e827dc28289e71d57465
Eshwarpawanpeddi <eshwarpavanpeddi@gmail.com>
2026-01-22 21:09:34 +0530

    first attempt

--- 2026-01-22/leet#11.py
+++ 2026-01-22/leet#11.py
@@ -1,4 +1,14 @@
 class Solution:
-    
+    def area(i,j):
+        return i*j
+
     def maxArea(self, height: List[int]) -> int:
-        
+        left = 0
+        right = 0
+        max_area = 0
+        for i in range(len(height)):
+            for j in range(len(height)):
+                temp_area = area(height[i], height[j])
+                if temp_area > max_area:
+                    max_area = temp_area
+        return max_area
