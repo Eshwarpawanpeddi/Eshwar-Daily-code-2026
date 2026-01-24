98229ced1225240b12e1cb510b625d00f46f5784
Eshwarpawanpeddi <eshwarpavanpeddi@gmail.com>
2026-01-24 21:33:13 +0530

    first attempt

--- 2026-01-24/leet#14.py
+++ 2026-01-24/leet#14.py
@@ -1,3 +1,17 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        
+
+        while 1<= len(strs) <= 200 and all(1 <= len(s) <= 200 for s in strs):
+            longest_suffix = []
+
+            for i in range(len(strs)):
+                if len(strs[i]) < j:
+                    strs[i] = strs[i].ljust(j, ' ')
+
+            for words in strs:
+                if words[i][j] == words[i+1][j] == words[i+2][j]:
+                    longest_suffix.append(words[i][j])
+                    j += 1
+                else:
+                    break
+            return ''.join(longest_suffix)
