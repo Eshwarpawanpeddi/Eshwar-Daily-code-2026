a7b95a2c3f356154ebc58b2281b4dd31dcf44b1b
Eshwarpawanpeddi <eshwarpavanpeddi@gmail.com>
2026-01-25 19:10:25 +0530

    attempt 1

--- 2026-01-25/leet#15.py
+++ 2026-01-25/leet#15.py
@@ -1,3 +1,17 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i == j:
+                    continue
+                else:
+                    for k in range(len(nums)):
+                        if k == i or k == j:
+                            continue
+                        else:
+                            if nums[i] + nums[j] + nums[k] == 0:
+                                triplet = sorted([nums[i], nums[j], nums[k]])
+                                if triplet not in self.result:
+                                    self.result.append(triplet)
+        return triplet
+    
